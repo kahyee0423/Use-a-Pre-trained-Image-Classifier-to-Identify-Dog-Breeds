d2ffb575b7c3bca1950f4dd67933598a43ac1b35
Hor Kah Yee <161730968+kahyee0423@users.noreply.github.com>
2024-11-22 16:41:50 +0800

    Update print_functions_for_lab_checks.py

--- print_functions_for_lab_checks.py
+++ print_functions_for_lab_checks.py
@@ -6,29 +6,9 @@
 # DATE CREATED: 05/14/2018                                  
 # REVISED DATE:             <=(Date Revised - if any)                         
 # PURPOSE:  This set of functions can be used to check your code after programming 
-#           each function. The top section of each part of the lab contains
-#           the section labeled 'Checking your code'. When directed within this
-#           section of the lab one can use these functions to more easily check 
-#           your code. See the docstrings below each function for details on how
-#           to use the function within your code.
-#
-##
+#           each function.
 
-# Functions below defined to help with "Checking your code", specifically
-# running these functions with the appropriate input arguments within the
-# main() funtion will print out what's needed for "Checking your code"
-#
 def check_command_line_arguments(in_arg):
-    """
-    For Lab: Classifying Images - 7. Command Line Arguments
-    Prints each of the command line arguments passed in as parameter in_arg, 
-    assumes you defined all three command line arguments as outlined in 
-    '7. Command Line Arguments'
-    Parameters:
-     in_arg -data structure that stores the command line arguments object
-    Returns:
-     Nothing - just prints to console  
-    """
     if in_arg is None:
         print("* Doesn't Check the Command Line Arguments because 'get_input_args' hasn't been defined.")
     else:
@@ -37,17 +17,6 @@ def check_command_line_arguments(in_arg):
               "\n    arch =", in_arg.arch, "\n dogfile =", in_arg.dogfile)
 
 def check_creating_pet_image_labels(results_dic):
-    """    For Lab: Classifying Images - 9/10. Creating Pet Image Labels
-    Prints first 10 key-value pairs and makes sure there are 40 key-value 
-    pairs in your results_dic dictionary. Assumes you defined the results_dic
-    dictionary as was outlined in 
-    '9/10. Creating Pet Image Labels'
-    Parameters:
-      results_dic - Dictionary with key as image filename and value as a List 
-             (index)idx 0 = pet image label (string)
-    Returns:
-     Nothing - just prints to console  
-    """
     if results_dic is None:
         print("* Doesn't Check the Results Dictionary because 'get_pet_labels' hasn't been defined.")
     else:
@@ -79,22 +48,6 @@ def check_creating_pet_image_labels(results_dic):
 
 
 def check_classifying_images(results_dic):
-    """    For Lab: Classifying Images - 11/12. Classifying Images
-    Prints Pet Image Label and Classifier Label for ALL Matches followed by ALL 
-    NOT matches. Next prints out the total number of images followed by how 
-    many were matches and how many were not-matches to check all 40 images are
-    processed. Assumes you defined the results_dic dictionary as was 
-    outlined in '11/12. Classifying Images'
-    Parameters:
-      results_dic - Dictionary with key as image filename and value as a List 
-             (index)idx 0 = pet image label (string)
-                    idx 1 = classifier label (string)
-                    idx 2 = 1/0 (int)   where 1 = match between pet image and 
-                    classifer labels and 0 = no match between labels
-    Returns:
-     Nothing - just prints to console  
-
-    """
     if results_dic is None:
         print("* Doesn't Check the Results Dictionary because 'classify_images' hasn't been defined.")
     elif len(results_dic[next(iter(results_dic))]) < 2:
@@ -138,28 +91,6 @@ def check_classifying_images(results_dic):
 
  
 def check_classifying_labels_as_dogs(results_dic):
-    """    For Lab: Classifying Images - 13. Classifying Labels as Dogs
-    Prints Pet Image Label, Classifier Label, whether Pet Label is-a-dog(1=Yes,
-    0=No), and whether Classifier Label is-a-dog(1=Yes, 0=No) for ALL Matches 
-    followed by ALL NOT matches. Next prints out the total number of images 
-    followed by how many were matches and how many were not-matches to check 
-    all 40 images are processed. Assumes you defined the results_dic dictionary
-    as was outlined in '13. Classifying Labels as Dogs'
-    Parameters:
-      results_dic - Dictionary with key as image filename and value as a List 
-             (index)idx 0 = pet image label (string)
-                    idx 1 = classifier label (string)
-                    idx 2 = 1/0 (int)   where 1 = match between pet image and 
-                    classifer labels and 0 = no match between labels
-                    idx 3 = 1/0 (int)  where 1 = pet image 'is-a' dog and 
-                            0 = pet Image 'is-NOT-a' dog. 
-                    idx 4 = 1/0 (int)  where 1 = Classifier classifies image 
-                            'as-a' dog and 0 = Classifier classifies image  
-                            'as-NOT-a' dog.
-    Returns:
-     Nothing - just prints to console  
-
-    """
     if results_dic is None:
         print("* Doesn't Check the Results Dictionary because 'adjust_results4_isadog' hasn't been defined.")
     elif len(results_dic[next(iter(results_dic))]) < 4 :
@@ -208,31 +139,6 @@ def check_classifying_labels_as_dogs(results_dic):
 
 
 def check_calculating_results(results_dic, results_stats_dic):
-    """    For Lab: Classifying Images - 14. Calculating Results
-    Prints First statistics from the results stats dictionary (that was created
-    by the calculates_results_stats() function), then prints the same statistics
-    that were calculated in this function using the results dictionary.
-    Assumes you defined the results_stats dictionary and the statistics 
-    as was outlined in '14. Calculating Results '
-    Parameters:
-      results_dic - Dictionary with key as image filename and value as a List 
-             (index)idx 0 = pet image label (string)
-                    idx 1 = classifier label (string)
-                    idx 2 = 1/0 (int)   where 1 = match between pet image and 
-                    classifer labels and 0 = no match between labels
-                    idx 3 = 1/0 (int)  where 1 = pet image 'is-a' dog and 
-                            0 = pet Image 'is-NOT-a' dog. 
-                    idx 4 = 1/0 (int)  where 1 = Classifier classifies image 
-                            'as-a' dog and 0 = Classifier classifies image  
-                            'as-NOT-a' dog.
-     results_stats_dic - Dictionary that contains the results statistics (either
-                     a percentage or a count) where the key is the statistic's 
-                     name (starting with 'pct' for percentage or 'n' for count)
-                     and the value is the statistic's value 
-    Returns:
-     Nothing - just prints to console  
-
-    """
     if results_stats_dic is None:
         print("* Doesn't Check the Results Dictionary because 'calculates_results_stats' hasn't been defined.")
     else:
